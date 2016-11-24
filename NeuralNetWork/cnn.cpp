eb5f113aa1f60933ff3abfbee1bc7eab53b86185
toroumm <toroumm@gmail.com>
2016-11-24 14:19:48 -0200

    update 24-11-2016

--- NeuralNetWork/cnn.cpp
+++ NeuralNetWork/cnn.cpp
@@ -67,16 +67,82 @@ int CNN_TUPLE::get_frame_size(int size, int _overlap){
 
 //****************************************************************************
 
-CNN::CNN(int height, int width, int n_frames, int neighbors = 8, int _overlap = 0){
+CNN::CNN(CONFIG_CNN configuration){
 
-    int size = height*width;
-    for(int i = 0;i < _layers; i++){
 
-        hidden_layers.push_back(CNN_TUPLE(size,n_frames,neighbors, _overlap));
-    }
 
 }
 //****************************************************************************
+/*
+CONF_CNN_LAYER::CONF_CNN_LAYER(){
 
+    set_full_connect(1);
+    set_n_frames(10);
+    set_window_height(5);
+    set_window_width(5);
+
+}*/
+/**
+ * @brief CONF_CNN_LAYER::CONF_CNN_LAYER: This Contrututor make a model of each tuple convolution layer.
+ * @param _n_frames : int Number of frames on this layer
+ * @param _height : int Height's of convolution window
+ * @param _width : int  WIdths's of convolution window
+ * @param _overlap : int Overlap on complex window convolution
+ * @param _full_connection: bool
+ */
+
+CONF_CNN_LAYER::CONF_CNN_LAYER(int _n_frames, int _c_height, int _c_width, int _s_height, int _s_width, int _overlap  ,bool _full_connection ){
+
+    set_full_connect(_full_connection);
+    set_n_frames(_n_frames);
+    set_complex_window_height(_c_height);
+    set_complex_window_width(_c_width);
+    set_simplex_window_height(_s_height);
+    set_simplex_window_width(_s_width);
+    set_overlap(_overlap);
+}
+
+CONF_CNN_LAYER::set_full_connect(bool value){full = value;}
+CONF_CNN_LAYER::set_layer_connection(vector<int> frames){set_full_connect(0);connection_conf = frames;}
+CONF_CNN_LAYER::set_n_frames(int value){n_frames =value;}
+CONF_CNN_LAYER::set_overlap(int value){overlap =value;}
+CONF_CNN_LAYER::set_complex_window_height(int value){c_height = value;}
+CONF_CNN_LAYER::set_complex_window_width(int value){c_width =value;}
+CONF_CNN_LAYER::set_simplex_window_height(int value){s_height =value;}
+CONF_CNN_LAYER::set_simplex_window_width(int value){s_width = value;}
+
+
+bool CONF_CNN_LAYER::get_n_frames(){return n_frames;}
+vector<int> CONF_CNN_LAYER::get_connections(){return connection_conf;}
+int CONF_CNN_LAYER::get_overlap(){return overlap;}
+int CONF_CNN_LAYER::get_complex_window_height(){return c_height;}
+int CONF_CNN_LAYER::get_complex_window_width(){return c_width;}
+int CONF_CNN_LAYER::get_simplex_window_height(){return s_height;}
+int CONF_CNN_LAYER::get_simplex_window_width(){return s_width;}
+
+
+/**
+ * @brief CONFIG_CNN::CONFIG_CNN This contructor start the setup of each convolution layer of model
+ * @param n_layer : Tuple's number of your convolution model
+ */
+
+CONFIG_CNN::CONFIG_CNN(int n_layer){
+
+    cnn_layer.resize(n_layer);
+}
+
+/**
+ * @brief CONFIG_CNN::set_layer : Give access to any tuple to setup
+ * @param number : This parameter it's a number of tuple which you need setup
+ * @return desire tuple
+ */
+
+vector<CONF_CNN_LAYER> CONFIG_CNN::set_layer(int number){
+    if(number>0 && number < cnn_layer.size())
+        return cnn_layer[number];
+    else
+         return 0;
+}
+//****************************************************************************
 
 
--- NeuralNetWork/cnn.h
+++ NeuralNetWork/cnn.h
@@ -61,7 +61,7 @@ private:
 class CNN: public Nets{
 
 public:
-    CNN(int height, int width, int n_frames, int neighbors = 8, int neighbors = 8, int _overlap = 0 );
+    CNN(int height, int width, int neighbors = 8, int _overlap = 0 );
 
     vector<CNN_TUPLE>hidden_layers;
     Layer output_layer; //verificar std::pair
@@ -70,6 +70,56 @@ public:
 private:
 
 protected:
+
+};
+
+class CONF_CNN_LAYER{
+
+public:
+
+    //CONF_CNN_LAYER();
+    CONF_CNN_LAYER(int _n_frames =10, int _c_height =5, int _c_width = 5, int _s_height =1, int _s_width=1, int _overlap =1 ,bool _full_connection = false);
+
+    int get_n_frames();
+    int get_complex_window_height();
+    int get_complex_window_width();
+    int get_simplex_window_height();
+    int get_simplex_window_width();
+    int get_overlap();
+    vector<int>get_connections();
+
+    void set_n_frames(int value);
+    void set_complex_window_height(int value);
+    void set_complex_window_width(int value);
+    void set_simplex_window_height(int value);
+    void set_simplex_window_width(int value);
+    void set_overlap(int value);
+    void set_full_connect(bool value =1);
+    void set_layer_connection(vector<int>frames);
+
+ private:
+
+    int n_frames, c_height, c_width, s_height, s_width, overlap;
+    bool full;
+    vector<int>connection_conf;
+
+};
+
+
+
+class CONFIG_CNN{
+
+public:
+
+    CONFIG_CNN(int n_layer);
+
+    vector<CONF_CNN_LAYER>set_layer(int number);
+
+
+private:
+
+    vector<CONF_CNN_LAYER>cnn_layer;
+
 
 };
 
